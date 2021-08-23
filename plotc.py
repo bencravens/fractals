6330cd0bbb0f1dfa49a9385a4b5062ba383e8a8d
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2021-08-24 11:37:59 +1200

    Update plotc.py

--- plotc.py
+++ plotc.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
 
-for i in range(15):
+for i in range(35):
     arr = np.log(np.loadtxt("{}.csv".format(i),delimiter=','))
     plt.pcolor(arr)
     plt.savefig("{}.png".format(i))
