12f143e4d0064a62d83adac999a6b0ac2ebe9c42
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2021-08-25 16:27:10 +1200

    Delete plotc.py

--- plotc.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-from PIL import Image
-
-for i in range(35):
-    arr = np.log(np.loadtxt("{}.csv".format(i),delimiter=','))
-    plt.pcolor(arr)
-    plt.savefig("{}.png".format(i))
-    plt.close()
