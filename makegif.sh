fe7f87b8b223c40bd9b1f9c12d777d1e0db5143c
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2021-08-26 14:44:21 +1200

    Add files via upload

--- makegif.sh
+++ makegif.sh
@@ -1,5 +1,18 @@
-#bash file to get around make's tab formatting
+#!/bin/bash
 
+#run fractal generation program. saves as .pgm files
 gcc -W -Wall -O2 -g fractal_zoom.c -o out
 ./out
-convert -delay 10 -loop 0 `ls -v *.pgm` fractal_zoom.gif 
+
+#convert all .pgm files to .png files
+num_files=`find ./ -name '*.pgm' | wc -l`
+#0 based indexing
+num_files=$((num_files-1))
+for file in $(seq 0 $num_files)
+do
+    echo "converting $file.pgm to $file.png"
+    convert $file.pgm $file.png
+done
+
+#now convert png files to a gif
+convert -delay 10 -loop 0 `ls *.png | sort -V` fractal_zoom.gif 
